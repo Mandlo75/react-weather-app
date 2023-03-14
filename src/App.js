757f0caacf7fd52c2389d2cefa05846ca5a1103b
macc80 <96655607+macc80@users.noreply.github.com>
2023-03-14 10:53:28 +0200

    added github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/sukoluhle-sifani-73b42519a/"
+          target="_blank"
+        >
+          {" "}
+          Sukoluhle Sifani
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Mandlo75/react-weather-app" target="_blank">
+          open sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
