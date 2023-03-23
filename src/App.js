25a1505fbf59e55d6b668c516bbb0e2be287ce13
macc80 <96655607+macc80@users.noreply.github.com>
2023-03-23 14:11:16 +0200

    added css components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/sukoluhle-sifani-73b42519a/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Sukoluhle Sifani
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Mandlo75/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App </h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/sukoluhle-sifani-73b42519a/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Sukoluhle Sifani
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Mandlo75/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
