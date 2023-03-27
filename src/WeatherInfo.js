823ea8da7fe6f4cdb97a1da42aaa702474732e0a
macc80 <96655607+macc80@users.noreply.github.com>
2023-03-27 14:31:12 +0200

    created a new component WeatherTemperature and updated changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormartedDate from "./FormartedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,12 +15,9 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon}  />
+          <WeatherIcon code={props.data.icon} />
 
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="units">°C|F</span>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
