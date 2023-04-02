20cf0b6d6391f9de8a807a1b413e1a75facdb361
macc80 <96655607+macc80@users.noreply.github.com>
2023-04-02 21:42:37 +0200

    set the weather forecast to update the daily forecasts

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,41 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span>
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="units">
-          °C |{" "}
-          <a href="/" onClick={showFarenheit}>
-            °F
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    let farenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <span>
-        <span className="temperature">{Math.round(farenheit)}</span>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span>
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="units">°C</span>
+    </span>
+  );
 }
